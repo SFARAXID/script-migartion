672d2b5c75afce83b694e36542ff977f456e9cc5
SFARAXID <37905153+SFARAXID@users.noreply.github.com>
2018-06-11 10:40:57 +0000

    Update tables.php

--- inc/tables.php
+++ inc/tables.php
@@ -7,21 +7,10 @@ function addOldPrefix(& $element) {
 }
 
 /**
- *
+ * 
 */
 $tableChange = [
-    'icagenda_category',
-    'icagenda_customfields',
-    'icagenda_events',
-    'icagenda_registration',
-    'kunena_categories',
-    'kunena_users',
-    'menu_types',
-    'neorecruit_applications',
-    'neorecruit_offers',
-    'neorecruit_settings',
-    'phocagallery',
-    'phocagallery_categories'
+    'tableName'
 ];
 
 /**
@@ -33,25 +22,5 @@ $uselessTableLike = OLD_PREFIX . 'finder%';
  *
  */
 $uselessTables = [
-    'ucm_history',
-    'session',
-    'extensions',
-    'assets',
-    'languages',
-    'modules',
-    'modules_menu',
-    'schemas',
-    'session',
-    'template_styles',
-    'update_sites',
-    'update_sites_extensions',
-    'redirect_links',
-    'icagenda',
-    'kunena_ranks',
-    'kunena_smileys',
-    'kunena_topics',
-    'kunena_version',
-    'phocagallery_styles',
-    'postinstall_messages',
-    'tags'
+    'TableName'
 ];
